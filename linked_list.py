24d3619513da165e8005f684ea4aaeed2fdd86dc
Noah Shuart <shuart.noah@gmail.com>
2018-11-25 19:52:15 -0700

    Initial commit

---
+++ linked_list.py
@@ -0,0 +1,48 @@
+from collections.abc import MutableSequence
+
+class LinkedNode:
+    def __init__(self, value, next_node):
+        self.__next = next_node
+        self.__value = value
+    
+    def value():
+        return self.__value
+    
+    def next():
+        return self.__next
+
+class LinkedList(MutableSequence):
+    def __init__(self):
+        self.__head = None
+        self.__len = 0
+    
+    def __len__(self):
+        return self.__len
+    
+    def __iter__(self):
+        next_node = self.head
+        while next_node is not None:
+            yield next_node.value()
+            next_node = next_node.next()
+    
+    def __getitem__(self, index):
+        pass
+    
+    def __setitem__(self, index, value):
+        pass
+
+    def __delitem__(self, index):
+        pass
+    
+    def insert(self, value):
+        pass
+
+test_list = LinkedList()
+
+for i in 0..10:
+    test_list.insert(i)
+
+print test_list[0]
+
+for x in test_list:
+    print x
